3eccfe21cf3bf4328bb5e7f8db04ed0a861b76bd
ArthurFerrao <arthurdeferrao@gmail.com>
2018-08-21 10:20:10 -0300

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import APP_CONFIG from './app.config';
+import { Node, Link } from './d3';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+  nodes: Node[] = [];
+  links: Link[] = [];
+
+  constructor() {
+
+    /** constructing the nodes array */
+    
+    this.nodes.push(new Node(1,0,20));
+    this.nodes.push(new Node(2,200,20));
+    this.nodes.push(new Node(3,400, 20));
+   
+
+    /** connecting the nodes before starting the simulation */
+    this.links.push(new Link(1, 2));
+    this.links.push(new Link(3, 2));
+      
+    
+  }
+}
